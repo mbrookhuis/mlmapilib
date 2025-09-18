15c90cbb399f4b2a6d70829c505627dd1826f02c
Michael Brookhuis <michael.brookhuis@suse.com>
2025-09-18 20:30:27 +0200

    fixed logger

--- pkg/util/logger/logger.go
+++ pkg/util/logger/logger.go
@@ -8,8 +8,11 @@ import (
 	model "github.com/mbrookhuis/mlmapilib/pkg/models/inputfile"
 
 	"github.com/sirupsen/logrus"
+	// "mlm-autoconfig/pkg/util/constants"
+	// ri "mlm-autoconfig/pkg/util/readconfig"
 	"strings"
 	"sync"
+	// "mlm-autoconfig/pkg/config" // Adjust import path
 )
 
 // Logger is the application's central logger.
@@ -17,39 +20,39 @@ var Logger *logrus.Logger
 var once sync.Once
 
 // InitLogger initializes the global logger based on the application configuration.
-func InitLogger(genConfig model.ConfigGeneral) error {
+func InitLogger(genConfig model.Config) error {
 	once.Do(func() {
 		Logger = logrus.New()
 		Logger.SetFormatter(&logrus.TextFormatter{
 			FullTimestamp: true,
 		})
 		// Configure screen output
-		screenLevel, err := logrus.ParseLevel(genConfig.General.Log.ScreenLevel)
+		screenLevel, err := logrus.ParseLevel(genConfig.LogLevel.Screen)
 		if err != nil {
-			fmt.Printf("Invalid screen log level '%s', defaulting to info: %v\n", genConfig.General.Log.ScreenLevel, err)
+			fmt.Printf("Invalid screen log level '%s', defaulting to info: %v\n", genConfig.LogLevel.Screen, err)
 			screenLevel = logrus.InfoLevel
 		}
 		Logger.SetOutput(os.Stdout) // Default output to screen
 		Logger.SetLevel(screenLevel)
 		// Configure file output if a file path is provided
-		if genConfig.General.Log.FilePath != "" {
-			fileLevel, err := logrus.ParseLevel(genConfig.General.Log.FileLevel)
+		if genConfig.Dirs.LogDir != "" {
+			fileLevel, err := logrus.ParseLevel(genConfig.LogLevel.File)
 			if err != nil {
-				fmt.Printf("Invalid file log level '%s', defaulting to debug: %v\n", genConfig.General.Log.FileLevel, err)
+				fmt.Printf("Invalid file log level '%s', defaulting to debug: %v\n", genConfig.LogLevel.File, err)
 				fileLevel = logrus.DebugLevel
 			}
 			logDir := ""
-			lastSlash := strings.LastIndex(genConfig.General.Log.FilePath, "/")
+			lastSlash := strings.LastIndex(genConfig.Dirs.LogDir, "/")
 			if lastSlash != -1 {
-				logDir = genConfig.General.Log.FilePath[:lastSlash]
+				logDir = genConfig.Dirs.LogDir[:lastSlash]
 				if err := os.MkdirAll(logDir, 0755); err != nil {
 					fmt.Printf("Failed to create log directory '%s': %v\n", logDir, err)
 					return
 				}
 			}
-			logFile, err := os.OpenFile(genConfig.General.Log.FilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+			logFile, err := os.OpenFile(genConfig.Dirs.LogDir, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 			if err != nil {
-				fmt.Printf("Failed to open log file '%s': %v\n", genConfig.General.Log.FilePath, err)
+				fmt.Printf("Failed to open log file '%s': %v\n", genConfig.Dirs.LogDir, err)
 				return
 			}
 			mw := io.MultiWriter(os.Stdout, logFile)
